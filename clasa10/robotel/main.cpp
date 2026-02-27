c05fb476a2e104bc56b8ed5f5f26f2d4c04694f0
Mirciok <renchez.mircea@prometeu.md>
2026-02-27 19:27:57 +0200

    Update main.cpp
    finally finished this problematic problem

--- clasa10/robotel/main.cpp
+++ clasa10/robotel/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <cmath>
+#include <vector>
 using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+void distanta(int n, int m, int noptim, int moptim, vector<vector<int>> &sala);
+int main(){
+    int n, m, td, ts, tm, noptim, moptim;
+    long long timp;
+    cin>>n>>m>>td>>ts>>tm;
+    timp=0;
+    vector<vector<int>> sala(n, vector<int>(m));
+    noptim=n/2+1;
+    moptim=m/2+1;
+    cout<<noptim<<" "<<moptim<<endl;
+    distanta(n, m, noptim, moptim, sala);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                timp=timp+sala[i][j]*td;
+            }
+        }
+    timp=timp*2+n*m*(ts+tm);
+    cout<<timp;
+}
+void distanta(int n, int m, int noptim, int moptim, vector<vector<int>> &sala){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            sala[i][j]=max(abs(i-(noptim-1)), abs(j-(moptim-1)));
+        }
+    }
 }
